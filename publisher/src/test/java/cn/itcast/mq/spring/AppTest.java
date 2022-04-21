c69666bfb1ee01c8d6cf98b130911ce7b0e52130
wuxiaoran <wxrbw@qq.com>
2022-04-21 12:03:32 +0800

    增加发送回调

---
+++ publisher/src/test/java/cn/itcast/mq/spring/AppTest.java
@@ -0,0 +1,28 @@
+package cn.itcast.mq.spring;
+
+import cn.itcast.mq.component.RabbitSender;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AppTest {
+
+  @Autowired
+  private RabbitSender rabbitSender;
+
+  @Test
+  public void testSender(){
+    Map<String, Object> props = new HashMap<>();
+    props.put("attr1", "12345");
+    props.put("attr2", "gfw");
+
+    rabbitSender.send("hello from test 1", props);
+  }
+}
